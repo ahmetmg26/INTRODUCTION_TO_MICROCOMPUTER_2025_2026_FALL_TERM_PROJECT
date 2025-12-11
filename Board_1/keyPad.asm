5d497356040b3981a2e667b317c798795550abdc
Ahmet Melih Gumus <ahmetmelihg26@gmail.com>
2025-12-11 16:07:22 +0300

    Keypad ve Main dosyasinda duzenlemeler yapildi

--- Board_1/keyPad.asm
+++ Board_1/keyPad.asm
@@ -1,62 +1,137 @@
 #include <xc.inc>
 
-; -- RAM ADRESLER? (20H'den ba?lat?l?r) --
-KeyData     EQU 0x20    ; Bas?lan tu?un de?eri (0-15 veya FFh: Tu? bas?lmad?)
-KeyTemp     EQU 0x21    ; Geçici kay?tç?
-delay_temp0 EQU 0x22    ; Gecikme sayac?
-delay_temp1 EQU 0x23    ; Gecikme sayac?
-
-; -- Reset Vektörü ve Program Ba?lang?c? --
-    PSECT resetVec, class=CODE, delta=2
-    ORG 0x00
-    GOTO MAIN
-
-    PSECT code, class=CODE, delta=2
+;GLOBAL FONKSIYONLARIN BILDIRIMI
+GLOBAL tus_tara   
+GLOBAL cevrim_tablosu
+GLOBAL tus_bekle_oku
+    
+;GLOBAL DEGISKENLER
+GLOBAL ham_tus
+GLOBAL temp_tus
+    
+    
+;LOCAL DEGISKENLER
+    
+    
+    
+;LOCAL degiskenlerin tanimlanmasi
+PSECT udata
  
- MAIN:
-    CALL INIT           ; Port ayarlar?
-    MOVLW 0xFF          ; Ba?lang?çta tu? bas?lmad?
-    MOVWF KeyData
 
-LOOP:
-    CLRWDT              ; Watchdog Timer'? temizle
-    CALL KEYPAD_SCAN    ; Keypad'i oku
+ 
+ 
+ 
+; Fonksiyonlar    
+PSECT code,class=CODE,delta=2
+ 
+tus_tara:
+    ; --- 1. SATIRI TARA (RB0 = 0) ---
+    MOVLW   11111110B    
+    MOVWF   PORTB
+    BTFSS   PORTB, 4        ; Sütun 1 (RB4) 0 m??
+    RETLW   0            ; Evet -> Tablodaki 0. eleman (Tu? '1')
+    BTFSS   PORTB, 5        ; Sütun 2 (RB5) 0 m??
+    RETLW   1            ; Evet -> Tablodaki 1. eleman (Tu? '2')
+    BTFSS   PORTB, 6        ; Sütun 3 (RB6) 0 m??
+    RETLW   2            ; Evet -> Tablodaki 2. eleman (Tu? '3')
+    BTFSS   PORTB, 7        ; Sütun 4 (RB7) 0 m??
+    RETLW   3            ; Evet -> Tablodaki 3. eleman (Tu? 'A')
 
-    ; Tu? de?erini kullanmak için buraya kod ekle
-    ; Örn: MOVF KeyData, W
-    ; E?er KeyData = 0x05 ise, '5' tu?u bas?lm?? demektir.
+    ; --- 2. SATIRI TARA (RB1 = 0) ---
+    MOVLW   11111101B
+    MOVWF   PORTB
+    BTFSS   PORTB, 4
+    RETLW   4            ; Tu? '4'
+    BTFSS   PORTB, 5
+    RETLW   5            ; Tu? '5'
+    BTFSS   PORTB, 6
+    RETLW   6           ; Tu? '6'
+    BTFSS   PORTB, 7
+    RETLW   7            ; Tu? 'B'
 
-    GOTO LOOP
+    ; --- 3. SATIRI TARA (RB2 = 0) ---
+    MOVLW   11111011B
+    MOVWF   PORTB
+    BTFSS   PORTB, 4
+    RETLW   8            ; Tu? '7'
+    BTFSS   PORTB, 5
+    RETLW   9            ; Tu? '8'
+    BTFSS   PORTB, 6
+    RETLW   10           ; Tu? '9'
+    BTFSS   PORTB, 7
+    RETLW   11           ; Tu? 'C'
+
+    ; --- 4. SATIRI TARA (RB3 = 0) ---
+    MOVLW   11110111B
+    MOVWF   PORTB
+    BTFSS   PORTB, 4
+    RETLW   12          ; Tu? '*'
+    BTFSS   PORTB, 5
+    RETLW   13           ; Tu? '0'
+    BTFSS   PORTB, 6
+    RETLW   14           ; Tu? '#'
+    BTFSS   PORTB, 7
+    RETLW   15           ; Tu? 'D'
+
+    RETLW   0xFF            ; H?ÇB?R?NE BASILMADIYSA FF ?LE DÖN ????????????????????????????????*
+    
+    ;------------------------------------------------------------------------------
+    
+cevrim_tablosu:
+    NOP
+    MOVWF   ham_tus         ; Ham de?eri sakla (Kaybolmas?n)
+    ADDWF   PCL, F          ; Program Sayac?na (PCL) W'yi ekle -> Z?plama yapar!
+    
+    ; Tablo: (Keypad üzerindeki fiziksel s?raya göre dönü?ecek de?erler)
+    ; S?ralama: 1, 2, 3, A, 4, 5, 6, B... ?eklinde gider.
+    
+    RETLW   0x01           ; 0. ?ndeks -> Tu? '1'
+    RETLW   0x02           ; 1. ?ndeks -> Tu? '2'
+    RETLW   0x03           ; 2. ?ndeks -> Tu? '3'
+    RETLW   0x0A          ; 3. ?ndeks -> Tu? 'A'
+    
+    RETLW   0x04           ; 4. ?ndeks -> Tu? '4'
+    RETLW   0x05           ; 5. ?ndeks -> Tu? '5'
+    RETLW   0x06           ; 6. ?ndeks -> Tu? '6'
+    RETLW   0x0B           ; 7. ?ndeks -> Tu? 'B'
+    
+    RETLW   0x07           ; 8. ?ndeks -> Tu? '7'
+    RETLW   0x08           ; 9. ?ndeks -> Tu? '8'
+    RETLW   0x09           ; 10. ?ndeks -> Tu? '9'
+    RETLW   0x0C           ; 11. ?ndeks -> Tu? 'C'
+    
+    RETLW   0x0E           ; 12. ?ndeks -> Tu? '*' (E harfi dedik örnek olarak)
+    RETLW   0x00           ; 13. ?ndeks -> Tu? '0'
+    RETLW   0x0F           ; 14. ?ndeks -> Tu? '#' (F harfi dedik)
+    RETLW   0x0D           ; 15. ?ndeks -> Tu? 'D'
     
     
-INIT:
-BSF STATUS, 5       ; Bank 1'e geç
-
-; TRISB Ayar?: 
-; RB7-RB4 (Sat?rlar) = Giri? (1)
-; RB3-RB0 (Sütunlar) = Ç?k?? (0)
-MOVLW 0xF0          ; 11110000B
-MOVWF TRISB
-
-; WPU Ayar?: Dahili Pull-up Dirençleri kullan?l?r
-; PORTB'deki tüm pinler için pull-up etkinle?tirilir.
-MOVLW 0x00
-MOVWF OPTION_REG    ; RBPU biti (Bit 7) 0'a çekilerek Pull-up etkinle?tirilir.
-
-BCF STATUS, 5       ; Bank 0'a geç
-
-CLRF PORTB          ; Ç?k?? pinlerini ba?lang?çta temizle
-RETURN
+    ;--------------------------------------------------------------
     
-DELAY_50MS:
-    MOVLW 0x20          ; D?? döngü (Yakla??k 32)
-    MOVWF delay_temp1
-D1_LOOP:
-    MOVLW 0xFF          ; ?ç döngü (255)
-    MOVWF delay_temp0
-D2_LOOP:
-    DECFSZ delay_temp0, F
-    GOTO D2_LOOP
-    DECFSZ delay_temp1, F
-    GOTO D1_LOOP
+    ;Tuþa basýlana kadar bekler, basýlýnca deðeri W'ye alýr,
+    ; parmak çekilene kadar bekler ve döner.
+tus_bekle_oku:
+    ;Basýlana kadar bekle
+    CALL    tus_tara
+    XORLW   0xFF
+    BTFSC   STATUS, 2       ; Z=1 ise (W=FF) tuþ yok demektir
+    ;GOTO    tus_bekle_oku   ; Tuþ yoksa tekrar tara
     RETURN
+    
+    ;Tuþu al ve sakla
+    CALL    tus_tara        ; Tekrar oku (W'de index var)
+    CALL    cevrim_tablosu  ; Gerçek deðere çevir (W'de A, *, 5 vs var)
+    MOVWF   temp_tus        ; Kaybetmemek için sakla
+
+    ; 3. Parmak çekilene kadar bekle (Debounce + Release)
+    TUS_BIRAKMA:
+	CALL    tus_tara
+	XORLW   0xFF
+	BTFSS   STATUS, 2       ; Z=0 ise (W!=FF) hala basýlý
+	GOTO    TUS_BIRAKMA     ; Býrakýlana kadar dön
+
+	; 4. Saklanan deðeri W'ye yükle ve dön
+	MOVF    temp_tus, W
+	RETURN
+    
+    
--- Board_1/test_main.asm
+++ Board_1/test_main.asm
@@ -4,6 +4,7 @@
 #include <xc.inc>
 #include "temperature_module.asm" 
 #include "sevenSegmentDisplay.asm"
+#include "keyPad.asm"
 
 
     GLOBAL MAIN
@@ -27,7 +28,13 @@
     GLOBAL digitTens
     GLOBAL digitHundreds
     GLOBAL digitThousands
-     
+    
+    ;Keypad degiskenleri
+    GLOBAL ham_tus
+    GLOBAL temp_tus
+    GLOBAL girilen_onlar
+    GLOBAL girilen_birler
+    GLOBAL girilen_ondalik
 
 ; Configuration bits
 CONFIG FOSC = XT
@@ -53,7 +60,7 @@ ORG 0x0000
 ;==========================================================
 PSECT udata_bank0,class=BANK0,space=1,delta=1,noexec
 
-; temperature modul icin degiskenler
+; temperature modul icin degiskenleri tanimla
 ambient_temp_int:      DS 1
 ambient_temp_frac:     DS 1
 desired_temp_int:      DS 1
@@ -64,7 +71,7 @@ temp_compare_result:   DS 1
     delay_inner:    DS 1
 tach_counter:       DS 1
 
-; 7-seg-display icin degiskenler
+; 7-seg-display icin degiskenleri tanimla
 digitNum:  DS 1
 digitOnes: DS 1
 digitTens: DS 1
@@ -75,13 +82,19 @@ onlar: DS 1
 delay5ms_temp0: DS 1
 delay5ms_temp1: DS 1
 
-
+; Keypad degiskeni tanýmla
+ham_tus: DS 1
+temp_tus: DS 1    
+girilen_onlar:  DS 1    ; Ýlk rakam (Örn: 2)
+girilen_birler: DS 1    ; Ýkinci rakam (Örn: 4)
+girilen_ondalik: DS 1   ; Virgülden sonraki rakam (Örn: 5)
+temp_calc: DS 1 ;    onlar ve birler'i desired_temp_int deðerinde toplamak için
 PSECT code
  
 MAIN:
     CALL PortInit
     
-    ; Ba?lang?ç de?erleri
+    ; Ba?lang?? de?erleri
     MOVLW 35
     BANKSEL desired_temp_int
     MOVWF desired_temp_int
@@ -91,7 +104,7 @@ READ_LOOP:
     CALL TEMP_ReadAmbient       ; temperature_module.asm'den
     CALL TEMP_UpdateFanControl      ; temperature_module.asm'den
     
-    ; Test: desired temp'i 7-seg-display'e göster
+    ; Test: desired temp'i 7-seg-display'e g?ster
     MOVF desired_temp_int,W ; W=desired_temp_int
     CALL digitlere_ayir ; burdan biler ve onlar belirlenir
     
@@ -126,9 +139,19 @@ READ_LOOP:
     MOVWF digitNum
     CALL updateDisplay
     CALL delay5ms
+    ;----------------------------------
+    ; keypad tarama 
     
-    ;san?r?m boyle
-    ;CALL Delay_100ms
+    CALL KEYPAD
+    ; eðer w=0 dönmüþse girilen formatta sorun vardir, tekrar A'ya basýlmasý istenir (Z = 1 )
+    ; w=1 dönmüþse format doðrudur, desired_int ve frac deðerlerine atama yapýlýr (Z=0
+    BTFSC STATUS,2 ; z=0 ise atla
+    GOTO READ_LOOP
+    ;degeri gonderme islemleri
+    CALL kontrol_ve_atama ; eger desired_temp degeri istenen aralýktaysa RAM'e atanýr
+    
+    
+    
     GOTO READ_LOOP
 
     
@@ -153,8 +176,18 @@ PortInit:
 
     ; ---- PORTB Ayar? ----
     BANKSEL TRISB
-    MOVLW 00000100B ; RB2 giri? (tach), RB0/RB1 ç?k??
+    MOVLW 00000100B ; RB2 giri? (tach), RB0/RB1 cikis --> temperature
+		    ; RB4/RB7 cikis --> keypad
     MOVWF TRISB
+    
+    BCF     OPTION_REG, 7  ;OPTION_REG yazmacýnýn 7. biti (RBPU) 0 olmalý. PULL-UP direnci icin
+    
+    
+    ; -- PORTC ayarlama -- 
+    BANKSEL TRISC
+    MOVLW 00001111 ; RC0/RC3 input  --> keypad icin
+    MOVWF TRISC
+    
     
     ; -- 7-seg-display icin PORTD ayarlari -- 
     BANKSEL TRISD
@@ -176,10 +209,10 @@ PortInit:
 PSECT code
 
 Delay_100ms:
-        MOVLW   200        ; d?? döngü
+        MOVLW   200        ; d?? d?ng?
         MOVWF   delay_outer
 Delay_Outer_Loop:
-        MOVLW   250        ; iç döngü
+        MOVLW   250        ; i? d?ng?
         MOVWF   delay_inner
 Delay_Inner_Loop:
         DECFSZ  delay_inner, F
@@ -206,4 +239,131 @@ delay5ms:
     NOP
     GOTO delay5ms_Loop1
 
+KEYPAD:
+    ADIM_1_A:
+	CALL    tus_bekle_oku   ; Bir tuþ bekle
+	SUBLW   0x0A            ; Girilen tuþ 'A' (0x0A) mý?
+	BTFSS   STATUS, 2       ; Sonuç 0 ise (Z=1) eþittir.
+	RETLW 0        ; Deðilse, tekrar A gelene kadar bekle. ~~
+
+	; -----------------------------------------------------------
+	; ADIM 2: BÝRÝNCÝ RAKAMI AL (ONLAR BASAMAÐI)
+	; -----------------------------------------------------------
+    ADIM_2_R1:
+	CALL    tus_bekle_oku   ; Tuþ bekle
+	MOVWF   girilen_onlar   ; Gelen tuþu sakla
+
+	; Kontrol: Basýlan tuþ rakam mý? (0-9 arasý mý?)
+	; 9'dan büyükse (A,B,C,D,*,#) hata var demektir, baþa dön.
+	SUBLW   0x09            ; 9 - W iþlemi
+	BTFSS   STATUS, 0       ; Carry=0 ise W > 9 demektir (Negatif sonuç)
+	RETLW 0        ; Rakam deðilse baþa dön (Reset)
+
+	; -----------------------------------------------------------
+	; ADIM 3: ÝKÝNCÝ RAKAMI AL (BÝRLER BASAMAÐI)
+	; -----------------------------------------------------------
+    ADIM_3_R2:
+	CALL    tus_bekle_oku
+	MOVWF   girilen_birler
+
+	; Kontrol: Rakam mý?
+	SUBLW   0x09
+	BTFSS   STATUS, 0       ; W > 9 ise
+	RETLW 0        ; Baþa dön
+
+	; -----------------------------------------------------------
+	; ADIM 4: '*' KARAKTERÝNÝ BEKLE (0x0E)
+	; -----------------------------------------------------------
+    ADIM_4_YILDIZ:
+	CALL    tus_bekle_oku
+	SUBLW   0x0E            ; '*' tuþu tablonda 0x0E mi?
+	BTFSS   STATUS, 2       ; Eþit mi?
+	RETLW 0        ; Deðilse baþa dön
+
+	; -----------------------------------------------------------
+	; ADIM 5: ÜÇÜNCÜ RAKAMI AL (ONDALIK KISIM)
+	; -----------------------------------------------------------
+    ADIM_5_R3:
+	CALL    tus_bekle_oku
+	MOVWF   girilen_ondalik
+
+	; Kontrol: Rakam mý?
+	SUBLW   0x09
+	BTFSS   STATUS, 0
+	RETLW 0
+
+	; -----------------------------------------------------------
+	; ADIM 6: '#' KARAKTERÝNÝ BEKLE (BÝTÝÞ) (0x0F)
+	; -----------------------------------------------------------
+    ADIM_6_KARE:
+	CALL    tus_bekle_oku
+	SUBLW   0x0F            ; '#' tuþu tablonda 0x0F mi?
+	BTFSS   STATUS, 2
+	RETLW 0        ; Deðilse baþa dön
+	;DOÐRU GÝRÝÞ YAPILDI!
+	RETLW 1
+    
+kontrol_ve_atama:
+	
+	; (X * 10) = (X * 8) + (X * 2)
+	
+	MOVF    girilen_onlar, W
+	MOVWF   temp_calc           ; temp = X
+	BCF     STATUS, 0
+	RLF     temp_calc, F        ; temp = 2X
+
+	MOVF    temp_calc, W        ; W = 2X (Bunu kenara, W'ye aldýk)
+
+	BCF     STATUS, 0
+	RLF     temp_calc, F        ; temp = 4X
+	BCF     STATUS, 0
+	RLF     temp_calc, F        ; temp = 8X
+
+	ADDWF   temp_calc, W        ; W = 2X + 8X = 10X (Onlar basamaðý tamam)
+
+	; Þimdi Birler basamaðýný ekle
+	ADDWF   girilen_birler, W   ; W = (Onlar*10) + Birler
+	MOVWF   temp_calc           ; Sonucu temp_calc'a kaydet (Örn: 25)
+
+    
+    ; 2. ADIM: LÝMÝT KONTROLÜ (Örnek: Min 18, Max 40 Derece)
+    	; Üst Limit Kontrolü (MAX 50 )
+	MOVF    temp_calc, W
+	SUBLW   50              
+	BTFSS   STATUS, 0           ; Eðer sonuç negatifse (C=0), sayý 50'tan büyüktür.
+	GOTO    HATA_DURUMU         ; 50'tan büyükse hataya git
+
+	; Alt Limit Kontrolü (MIN 10 )
+	MOVF    temp_calc, W
+	SUBLW   10               ; 10 - Sayý (Eðer Sayý > 10 ise C=0 olur, dikkat!)
+	BTFSC   STATUS, 0           ; C=1 ise (Sayý <= 17) hataya git
+	GOTO    HATA_DURUMU
+
+    
+    ; 3. ADIM: ATAMA (Deðerler geçerli)
+    
+    KAYDET_VE_CIK:
+	; Tamsayý kýsmýný kaydet
+	MOVF    temp_calc, W
+	MOVWF   desired_temp_int    ; Ana deðiþkenimize atadýk
+
+	; Ondalýk kýsmýný kaydet (Bunu kontrol etmeye gerek yok, 0-9 arasýdýr zaten)
+	MOVF    girilen_ondalik, W
+	MOVWF   desired_temp_frac   ; Ana ondalýk deðiþkene atadýk
+
+	; Ýþlem baþarýlý mesajý veya LCD güncellemesi buraya
+	RETURN ;Döngüye geri dön
+
+    ;--------------------------------------------------------------------------
+    ; HATA YÖNETÝMÝ
+    ;--------------------------------------------------------------------------
+    HATA_DURUMU:
+	; Kullanýcý saçma bir deðer girdi (Örn: 99 derece veya 05 derece)
+	; Burada bir hata LED'i yakabilirsin veya LCD'de "GECERSIZ" yazabilirsin.
+	; Þimdilik sadece deðerleri sýfýrlayýp baþa dönelim.
+	CLRF    desired_temp_int
+	CLRF    desired_temp_frac
+	RETURN
+    
+	
     END
